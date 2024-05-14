0c6b3d91fc54d9d40b04ab873f8953ebc150dd24
Gareth Ellis <63304340+clinicalpositioning@users.noreply.github.com>
2024-05-14 12:31:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
 
     const formData = new FormData(this);
 
-    fetch('https://cors-anywhere.herokuapp.com/https://hook.eu2.make.com/ob6jz9596rxmhwfvvf453liq7thcn8c2', {
+    fetch('https://hook.eu2.make.com/ob6jz9596rxmhwfvvf453liq7thcn8c2', {
         method: 'POST',
-        body: formData
+        body: formData,
+        mode: 'no-cors'
     })
-    .then(response => {
-        if (response.ok) {
-            alert('Form submitted successfully!');
-            window.location.href = 'thankyou.html';
-        } else {
-            throw new Error('Error submitting the form.');
-        }
+    .then(() => {
+        alert('Form submitted successfully!');
+        window.location.href = 'thankyou.html';
     })
     .catch(error => {
         console.error('Error:', error);
